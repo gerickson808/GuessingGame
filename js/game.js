fcf5f163e5dc2a3968fb5a6044016ed88b47a7a0
Gustaf Erickson <gerickson808@gmail.com>
2015-12-11 19:29:23 -0500

    Added previous guesses, closeness, enter to submit

--- js/game.js
+++ js/game.js
@@ -2,60 +2,89 @@ $(document).ready(function(){
 	$('#playAgain').hide().removeClass('hidden');
 	var correctAnswer = generateNumber();
 	var lives = 5;
+	var previousGuesses = [];
 	console.log(correctAnswer);  //DELETE THIS WHEN DONE TESTING
-	$('#submit').on('click',function(){
-		answer = submitGuess(correctAnswer);
-		switch (answer) {
-			case -1:
-				loseLife(lives);
-				higher();
-				wrongMessage();
-				lives -= 1;
-				break;
-			case 1:
-				loseLife(lives);
-				lower();
-				wrongMessage();
-				lives -=1;
-				break;
-			case 0:
-				winGame(correctAnswer);
-				break;
-			default:
-		}
-		console.log(lives); // DELETE AFTER TESTING
-		if (lives == 0){
-			loseGame(correctAnswer);
+	$('#submit').on('click', function(){
+		lives = gameAction(correctAnswer, lives, previousGuesses);
+	});
+	$(this).on('keypress',function(e){
+		if(e.keyCode == 13){
+			lives = gameAction(correctAnswer,lives, previousGuesses);
 		};
 	});
 	$('#play-again').on('click', function(){
 		lives = 5;
+		previousGuesses = [];
 		resetGame();
 		correctAnswer = generateNumber();
 	});
 });
 
+function gameAction(correctAnswer, lives, previousGuesses){
+	var answer = submitGuess(correctAnswer, previousGuesses);
+	if (answer===0){
+		winGame(correctAnswer);
+	}
+	else if (answer != undefined){
+		console.log(answer);
+		results(answer, previousGuesses);
+		loseLife(lives);
+		wrongMessage(previousGuesses, correctAnswer)
+		lives -=1;
+	};
+	console.log(lives); // DELETE AFTER TESTING
+	if (lives == 0){
+		loseGame(correctAnswer);
+	};
+	$('#guessInput').val('');
+	return lives;
+}
+
+function results(answer,previousGuesses){
+	var light = "";
+	var dim = ""
+	if(answer == 1){
+		light = "#lower";
+		dim = "#higher";
+	}
+	else{
+		light = "#higher";
+		dim = "#lower";
+	};
+	feedback(light, dim, previousGuesses);
+}
+
+function feedback(light, dim,previousGuesses){
+	$(light).removeClass('dimmed');
+	$(dim).addClass('dimmed');
+	$('.panel-body').text(previousGuesses.join(", "));
+}
 
 
 function generateNumber() {
 	return parseInt(Math.random() * 100 + 1);
 }
 
-function submitGuess(answer){
+function submitGuess(answer, previousGuesses){
 	var guess = $('#guessInput').val();
-	if (isInt(guess)){
-		if (guess==answer){
+	if (isInt(guess) && guess != ""){
+		if (contains(previousGuesses, guess)===true){
+			$("#message").text("You've already guessed that!")
+		}
+		else if (guess==answer){
 			return 0;
 		}
 		else if (guess < answer){
+			previousGuesses.push(guess);
 			return -1;
 		}
 		else {
+			previousGuesses.push(guess);
 			return 1;
 		};
 	}
 	else {
-		alert("Please guess an integer!");
+		$("#message").text("Please guess an integer!");
 	};
 }
 
@@ -84,23 +113,37 @@ function loseLife(lives){
 		$('.lives:eq('+(lives-1)+')').fadeOut('slow');
 }
 
-function wrongMessage(){
+function wrongMessage(previousGuesses, correctAnswer){
 	var messages = ['Think carefully.', 'Seize the moment.',
 	'Don\'t let your dreams be dreams.', 'Find your inner strength.',
 	'Think. Think!', 'Phone a friend?', 'It\'s all riding on this.',
 	'Just do it!', 'Attack!'];
+	var closeMsg = ['Not close at all.', 'Kinda close.', 'You\'re on fire!']
+	var difference = Math.abs(previousGuesses[previousGuesses.length - 1] - correctAnswer);
+	var closeness = 0;
 	var msgId = parseInt(Math.random() * 9);
-	$('#message').text(messages[msgId]);
+	if (difference > 25){
+		closeness = 0;
+	}
+	else if (difference > 10){
+		closeness = 1;
+	}
+	else{
+		closeness = 2;
+	};
+	var message = messages[msgId] + " " + closeMsg[closeness];
+	$('#message').text(message);
 }
 
 function loseGame(answer){
-	alert('You lose. The answer was '+answer+'.');
-	playAgain();
+	$('#message').text('You lose! The answer was '+answer+'.');
+	setTimeout(playAgain, 3000);
 }
 
 function winGame(answer){
-	alert('You did it! The answer was '+answer+'!')
-	playAgain();
+	$('#message').text('You did it! The answer was '+answer+'!')
+	setTimeout(playAgain, 3000);
+
 }
 
 function playAgain(){
@@ -113,9 +156,24 @@ function playAgain(){
 function resetGame(){
 	$('#playAgain').hide();
 	$('#mainContent').show();
+	$('.lives').show();
 	$('.panel').show();
 	$('#message').text("You have 5 tries.")
 	$('#higher').addClass('dimmed');
 	$('#lower').addClass('dimmed');
 	$('#guessInput').val('');
+	$('.panel-body').text('');
+}
+
+function contains(array, value) {
+	var contained = false;
+	for (var key in array){
+		if (!array.hasOwnProperty(key)){
+			continue;
+		};
+		if (array[key] == value){
+			contained = true;
+		};
+	};
+	return contained;
 }
